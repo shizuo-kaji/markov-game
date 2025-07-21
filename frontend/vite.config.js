3f7f21d2f12e90c7e357af964770b65dd850c7bc
S. Graiff <s-graiff@kyudai.jp>
2025-07-21 15:27:01 +0900

    Add server config with proxy settings in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: 8000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
 })
